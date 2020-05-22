78c4b82fee929b45c98c6cd95434453a0828793c
erwan-joly <erwan.joly@pushpay.com>
2020-05-22 18:16:46 +1200

    reformat

--- test/NosCore.Algorithm.Tests/DocumentationTest.cs
+++ test/NosCore.Algorithm.Tests/DocumentationTest.cs
@@ -162,7 +162,7 @@ namespace NosCore.Algorithm.Tests
             {
                 resultBuilder.AppendLine();
                 var result = reputationService.GetReputation(reput);
-                resultBuilder.AppendLine($"- {(byte)reput,2} {reput,16} - Min: {result.Item1} Max: {result.Item2}");
+                resultBuilder.AppendLine($"- {(byte)reput,2} {reput.ToString().PadRight(16)} - Min: {result.Item1} Max: {result.Item2}");
                 if (reput < ReputationType.GreenLegend)
                 {
                     Assert.AreEqual(reput, reputationService.GetLevelFromReputation(result.Item1));
@@ -183,7 +183,7 @@ namespace NosCore.Algorithm.Tests
             {
                 resultBuilder.AppendLine();
                 var result = dignityService.GetDignity(dignity);
-                resultBuilder.AppendLine($"- {(byte)dignity,2} {dignity,11} - Max: {result.Item1} Min: {result.Item2}");
+                resultBuilder.AppendLine($"- {(byte)dignity,2} {dignity.ToString().PadRight(11)} - Max: {result.Item1} Min: {result.Item2}");
                 Assert.AreEqual(dignity, dignityService.GetLevelFromDignity(result.Item1));
                 Assert.AreEqual(dignity, dignityService.GetLevelFromDignity(result.Item2));
             }
